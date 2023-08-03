15411fc4bb5477cd094194bfbc160dbd326cfcf5
Monali Valunj <monavalunj96@gmail.com>
2023-08-03 08:00:24 -0700

    intial commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// GET ADD EMPLOYEE FORM AND EMPLOYEE TABLE FROM THE DOM
+    let addForm = document.getElementById("addForm");
+    let empTable = document.getElementById("employees");
+// SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
+let count = 0;
+    let empHeader = document.getElementsByClassName("header-title");
+    empHeader.inserBefore(document.createTextNode("count:" + count));
+
+// ADD EMPLOYEE
+addForm.addEventListener('submit', (e) => {
+    // PREVENT FORM SUBMISSION
+    e.preventDefault();
+
+    // GET THE VALUES FROM THE TEXT BOXES
+    let id = document.getElementById("id").value;
+    let name = document.getElementById("name").value;
+    let extension = document.getElementById("extension").value;
+    let email = document.getElementById("email").value;
+    let department = document.getElementById("department").value;
+
+    // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
+    let row = empTable.insertRow(-1);
+
+    // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
+    
+    let cellID = row.insertCell(0);
+    let cellName = row.insertCell(1);
+    let cellExt = row.insertCell(2)
+    let cellEmail = row.insertCell(3)
+    let cellDept = row.insertCell(4)
+
+    // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
+    
+    cellID.appendChild(document.createTextNode(id));
+    cellName.appendChild(document.createTextNode(name));
+    cellExt.appendChild(document.createTextNode(extension));
+    cellEmail.appendChild(document.createTextNode(email));
+    cellDept.appendChild(document.createTextNode(department));
+    
+    // CREATE THE DELETE BUTTON
+    let deletBtn = document.createElement('button');
+    deletBtn.appendChild(document.createTextNode("X"));
+    let celldeletBtn = row.insertCell(5);
+    celldeletBtn.appendChild(deletBtn);
+
+    // RESET THE FORM
+    addForm.reset();
+
+    // SET FOCUS BACK TO THE ID TEXT BOX
+    document.getElementById("id").focus;
+
+    // INCREMENENT THE NUMBER OF EMPLOYEES IN THE TABLE
+    count++;
+
+});
+
+// DELETE EMPLOYEE
